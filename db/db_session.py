25d32dfc1e44b326697300146e13820a883d991a
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-21 13:06:55 +0300

    solving DB problem

--- db/db_session.py
+++ db/db_session.py
@@ -8,26 +8,19 @@ SqlAlchemyBase = dec.declarative_base()
 __factory = None
 
 
-def global_init(db_file):
+def create_session() -> Session:
     global __factory
 
     if __factory:
-        return
+        return __factory()
 
-    if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
-
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = 'sqlite:///db/tetris.db?check_same_thread=False'
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-    print(__factory)
 
     from . import __all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
 
-
-def create_session() -> Session:
-    global __factory
     return __factory()
--- main.py
+++ main.py
@@ -127,5 +127,4 @@ def clear():
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/tetris.db")
     app.run()
