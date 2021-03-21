5b164db4fd343bb33543b88302c6217dbccc0a10
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-21 12:52:53 +0300

    remove blueprint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
+import datetime
 from db import db_session
-import api
+from db.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '72697676798779827668'
@@ -14,7 +15,117 @@ def not_found(error):
     }), 404)
 
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    if not request.json:
+        return jsonify({
+            'result': 'Empty request'
+        })
+    elif not all(key in request.json for key in [
+        'nickname',
+        'password',
+    ]):
+        return jsonify({
+            'result': 'Bad request'
+        })
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(
+        User.nickname == request.json['nickname']
+    ).first()
+    if user:
+        return jsonify({
+            'result': 'Nickname is already exists'
+        })
+    user = User()
+    user.nickname = request.json['nickname']
+    user.created = str(datetime.datetime.now()).split('.')[0]
+    user.set_password(request.json['password'])
+    db_sess.add(user)
+    db_sess.commit()
+    return jsonify({
+        'result': 'OK',
+    })
+
+
+@app.route('/users/<nickname>', methods=['GET'])
+def login(nickname):
+    if not request.json:
+        return jsonify({
+            'result': 'Empty request'
+        })
+    elif not all(key in request.json for key in [
+        'password',
+    ]):
+        return jsonify({
+            'result': 'Bad request'
+        })
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.nickname == nickname).first()
+    if not user:
+        return jsonify({
+            'result': 'Not found'
+        })
+    if not user.check_password(request.json['password']):
+        return jsonify({
+            'result': 'Incorrect password'
+        })
+    return jsonify({
+        'result': 'OK',
+    })
+
+
+@app.route('/users/', methods=['GET'])
+def get_users():
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).all()
+    return jsonify({
+        'users': [user.to_dict(only=(
+            'nickname',
+            'rating',
+            'best',
+            'created'
+        )) for user in users],
+    })
+
+
+@app.route('/users/<nickname>', methods=['PUT'])
+def edit_user(nickname):
+    if not request.json:
+        return jsonify({
+            'result': 'Empty request'
+        })
+    elif not all(key in request.json for key in [
+        'game_result',
+    ]):
+        return jsonify({
+            'result': 'Bad request'
+        })
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.nickname == nickname).first()
+    if not user:
+        return jsonify({
+            'result': 'Not found'
+        })
+    user.rating += int(request.json['game_result'])
+    user.best = max(user.best, int(request.json['game_result']))
+    db_sess.commit()
+    return jsonify({
+        'result': 'OK'
+    })
+
+
+@app.route('/users/', methods=['DELETE'])
+def clear():
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).all()
+    for user in users:
+        db_sess.delete(user)
+        db_sess.commit()
+    return jsonify({
+        'result': 'OK'
+    })
+
+
 if __name__ == '__main__':
     db_session.global_init("db/tetris.db")
-    app.register_blueprint(api.blueprint)
     app.run()
--- tests.py
+++ tests.py
@@ -1,68 +1,50 @@
 from requests import get, post, put, delete
-import logging
-from threading import Thread
-import time
-from flask import Flask
-from db import db_session
-import api
 
+server = 'http://127.0.0.1:5000/users'
+# server = 'https://epic-tetris-server.herokuapp.com/api/users'
 
-def run_tests():
-    time.sleep(1)
-    server = 'http://127.0.0.1:8080/api/users'
+print('Create user')
+print(post(server, json={
+    'nickname': 'TEST_01',
+    'password': '12345'
+}).json())
+print(get(server).json())
 
-    print('Create user')
-    print(post(server, json={
-        'nickname': 'TEST_01',
-        'password': '12345'
-    }).json())
-    print(get(server).json())
+print('Create user with same nickname')
+print(post(server, json={
+    'nickname': 'TEST_01',
+    'password': '54321'
+}).json())
+print(get(server).json())
 
-    print('Create user with same nickname')
-    print(post(server, json={
-        'nickname': 'TEST_01',
-        'password': '54321'
-    }).json())
-    print(get(server).json())
+print('Login with incorrect password')
+print(get(server + '/TEST_01', json={
+    'password': '54321'
+}).json())
 
-    print('Login with incorrect password')
-    print(get(server + '/TEST_01', json={
-        'password': '54321'
-    }).json())
+print('Login with correct password')
+print(get(server + '/TEST_01', json={
+    'password': '12345'
+}).json())
 
-    print('Login with correct password')
-    print(get(server + '/TEST_01', json={
-        'password': '12345'
-    }).json())
+print('Add game result')
+print(put(server + '/TEST_01', json={
+    'game_result': '10'
+}).json())
+print(get(server).json())
 
-    print('Add game result')
-    print(put(server + '/TEST_01', json={
-        'game_result': '10'
-    }).json())
-    print(get(server).json())
+print('Add game result for non-existent user')
+print(put(server + '/TEST_02', json={
+    'game_result': '10'
+}).json())
+print(get(server).json())
 
-    print('Add game result for non-existent user')
-    print(put(server + '/TEST_02', json={
-        'game_result': '10'
-    }).json())
-    print(get(server).json())
+print('Add another game result')
+print(put(server + '/TEST_01', json={
+    'game_result': '10'
+}).json())
+print(get(server).json())
 
-    print('Add another game result')
-    print(put(server + '/TEST_01', json={
-        'game_result': '10'
-    }).json())
-    print(get(server).json())
-
-    print('Delete all users')
-    print(delete(server).json())
-    print(get(server).json())
-
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = '72697676798779827668'
-    app.register_blueprint(api.blueprint)
-    logging.getLogger('werkzeug').setLevel(logging.ERROR)
-    db_session.global_init("db/tetris.db")
-    Thread(target=run_tests).start()
-    app.run(host='127.0.0.1', port=8080)
+print('Delete all users')
+print(delete(server).json())
+print(get(server).json())
